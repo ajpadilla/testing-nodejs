1be26d2e3bc72b271c6fc7ae47a9a7f7d26b874d
Alvaro Padilla <ajpadilla88@gmail.com>
2024-02-04 12:29:03 -0500

    Commit Service

--- examples/api/src/services/books.service.js
+++ examples/api/src/services/books.service.js
@@ -3,7 +3,8 @@ const MongoLib = require('../lib/mongo.lib');
 class BooksService {
   constructor() {
     this.collection = 'books';
-    this.mongoDB = new MongoLib();
+    //this.mongoDB = new MongoLib();
+    this.mongoDB = null;
   }
 
   getBooks(query) {
