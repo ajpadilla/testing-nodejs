954927553186dd3d7eb47cee173d10c070f1b498
Alvaro Padilla <ajpadilla88@gmail.com>
2024-02-17 13:42:04 -0500

    Actualizando test

--- examples/api/e2e/books.e2e.js
+++ examples/api/e2e/books.e2e.js
@@ -1,17 +1,24 @@
 const resquest = require('supertest');
 const { generateManyBooks } = require('../src/fakes/book.fake');
 
-/*const mockGetAll = jest.fn();
+const mockGetAll = jest.fn();
 
 const createApp = require('../src/app');
 
 
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+
+/*jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
   getAll: mockGetAll,
   create: () => {},
-})));
+})));*/
 
+jest.mock('../src/lib/mongo.lib', () => {
+  const MongoLibFake = require("../src/fakes/mongo.lib.fake");
+  return jest.fn().mockImplementation(() => {
+    return new MongoLibFake();
+  });
+});
 
 describe('Test for hello endpoint', () => {
   let app = null;
@@ -22,6 +29,11 @@ describe('Test for hello endpoint', () => {
   beforeAll(() => {
     app = createApp();
     server = app.listen(3001);
+
+    /* jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+      getAll: mockGetAll,
+      create: () => {},
+    }))); */
   });
 
   afterAll(() => {
@@ -30,24 +42,16 @@ describe('Test for hello endpoint', () => {
 
   describe('test for [GET] /api/v1/books', () => {
     test('should retun a list books', () => {
-      const fakeBooks = generateManyBooks(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+     /* const fakeBooks = generateManyBooks(3);
+      mockGetAll.mockResolvedValue(fakeBooks);*/
       return resquest(app)
         .get('/api/v1/books')
         .expect(200)
         .then(({ body }) => {
-          console.log(body);
-          expect(body.length).toEqual(fakeBooks.length);
+          //console.log(body);
+          //expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(20);
         });
-    });
-  });
-});*/
-
-describe('Test for hello endpoint', () => {
-
-  describe('test for [GET] /api/v1/books', () => {
-    test('should retun a list books', () => {
-
     });
   });
 });
--- examples/api/e2e/hello.e2e.js
+++ examples/api/e2e/hello.e2e.js
@@ -1,26 +1,35 @@
 const resquest = require('supertest');
 
 const createApp = require('../src/app');
+
+
+jest.mock('../src/lib/mongo.lib', () => {
+  const MongoLibFake = require("../src/fakes/mongo.lib.fake");
+  return jest.fn().mockImplementation(() => {
+    return new MongoLibFake();
+  });
+});
+
 describe('Test for hello endpoint', () => {
   let app = null;
   let server = null;
   beforeAll(() => {
     app = createApp();
-    server = app.listen(3001);
+    server = app.listen(3002);
   });
 
   afterAll(() => {
     server.close();
   });
 
-  /*describe('test for [GET] /', () => {
+  describe('test for [GET] /', () => {
     test('should retun "Hello World!"', () => resquest(app)
       .get('/')
       .expect(200)
       .then((response) => {
         expect(response.text).toEqual('Hello World!');
       }));
-  });*/
+  });
 
   describe('test for getBooks', () => {
     test('Should return a list book', async () => {
